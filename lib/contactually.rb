c60c55ef8061e32dab88c063fdb74a5b708ce76e
EBashkoff <eric@fivestreet.com>
2015-02-21 07:18:21 -0500

    Memoize and check contactually methods and actions.

--- lib/contactually.rb
+++ lib/contactually.rb
@@ -4,7 +4,12 @@ require "json"
 module Contactually
   class API
 
-    def initialize(api_key, headers = {} )
+    HTTP_ACTIONS = %w(get post put delete).freeze
+    METHODS      = %w(contacts notes groupings).freeze
+
+    attr_reader :http_action, :contactually_method
+
+    def initialize(api_key, headers = {})
       @api_key = api_key
 
       # The Contactually API recommends passing in a User-Agent header unique to
@@ -18,42 +23,48 @@ module Contactually
       @headers = headers
     end
 
-    def call(method, args)
-      parsed_response = make_call(method, args)
-      parsed_response
+    def call(method, params)
+      if (parsed_response = make_call(method, params))
+        parsed_response
+      else
+        false
+      end
     end
 
     def method_missing(method, *args)
-      call(method, args)
+      call(method, args.try(:first) || {})
     end
 
     private
-    def param_fields(args)
-      params = {}
-      params = args.first.merge(params) if args.first
+
+    def param_fields(params)
+      return params.to_json unless %w(get delete).include?(http_action)
       params
     end
 
-    def make_call(method, args={})
-      http_method, contactually_method = get_methods(method)
-      uri = build_uri(contactually_method, args)
-      response = Curl.send(http_method.to_sym, uri, JSON.dump(param_fields(args))) do |curl|
+    def make_call(composite, params = {})
+      return false unless get_methods(composite)
+      uri      = build_uri(contactually_method, params)
+      response = Curl.send(http_action.to_sym, uri, param_fields(params)) do |curl|
         @headers.each do |header_name, header_value|
           curl.headers[header_name.to_s.titleize.gsub(" ", "-")] = header_value
         end
-        curl.headers['Accept'] = 'application/json'
+        curl.headers['Accept']       = 'application/json'
         curl.headers['Content-Type'] = 'application/json'
       end
       JSON.load("[#{response.body_str}]").first
     end
 
-    def build_uri(contactually_method, args={})
-      #return "https://www.contactually.com/api/v1/#{contactually_method}/#{args[:id]}.json" if args[:id]
-      "https://www.contactually.com/api/v1/#{contactually_method}.json?api_key=#{@api_key}"
+    def build_uri(contactually_method, args = {})
+      id_arg = args.has_key?(:id) ? "/#{args["id"]}" : ""
+      "https://www.contactually.com/api/v1/#{contactually_method}#{id_arg}.json?api_key=#{@api_key}"
     end
 
-    def get_methods(method)
-      methods = method.to_s.split("_", 2)
+    def get_methods(composite)
+      http_action, contactually_method = composite.to_s.split("_", 2)
+      return false unless HTTP_ACTIONS.include?(http_action)
+      return false unless METHODS.include?(contactually_method)
+      [@http_action = http_action, @contactually_method = contactually_method]
     end
 
   end
